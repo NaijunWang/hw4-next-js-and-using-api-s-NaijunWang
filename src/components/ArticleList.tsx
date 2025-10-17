8a86a55228ff3bdfa83fc81ccd96cd80a7a9979e
NaijunWang <wnj123e@gmail.com>
2025-10-16 20:39:15 -0400

    feat: finish ArticleList component

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Card, Row, Skeleton } from "antd";
+import { Card, Row, Col, Skeleton } from "antd";
 import { Article } from "@/types/types";
 import ArticleCard from "./ArticleCard";
 
-// You'll need to replace undefined with the correct type
-const ArticleList: React.FC<undefined> = () => (
+interface ArticleListProps {
+  articles: Article[];
+  loading: boolean;
+}
+
+const ArticleList: React.FC<ArticleListProps> = ({ articles, loading }) => {
   /**
    * This component renders a list of articles. It takes as input an array of articles and a boolean indicating whether the list is loading.
    * You will need to write a props interface for this component.
@@ -15,7 +19,34 @@ const ArticleList: React.FC<undefined> = () => (
    * Don't forget to add a Skeleton component for when the list is loading. You might need conditional render logic for this
    *
    */
-  <div>Build me</div>
-);
+  if (loading){
+    return (
+      <Row gutter={[8,8]} justify="start">
+        {[0,1,2,3,4,5,6,7,8,9].map((_, index) => (
+          <Col
+            key={index}
+            span={8}
+          >
+            <Card style={{ width: "100%", borderRadius: 7 }}>
+              <Skeleton active paragraph={{ rows: 5 }} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+  return (
+    <Row gutter={[8, 8]} justify="start">
+      {articles.map((articles) => (
+        <Col
+          key={articles.id}
+          span={8}
+        >
+          <ArticleCard article={articles} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default ArticleList;
