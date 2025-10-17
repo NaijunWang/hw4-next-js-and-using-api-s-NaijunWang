8feac025ff188c8b007666b7a24ac96dea57f689
NaijunWang <wnj123e@gmail.com>
2025-10-16 21:47:01 -0400

    feat: finish Switch Button

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
-import { Divider, Typography } from "antd";
+import { Divider, Typography, Switch } from "antd";
 import { Article, ArticleResponse } from "@/types/types";
 import ArticleList from "@/components/ArticleList";
+import ArticleTable from "@/components/ArticleTable";
 
 const NewsPage: React.FC = () => {
   const [articles, setArticles] = useState<Article[]>([])
   const [loading, setLoading] = useState(true);
+  const [view, setView] = useState<"grid"|"table">("grid");
   useEffect(() => {
     async function getArticles(){
       const res = await fetch("https://api.spaceflightnewsapi.net/v4/articles/?limit=10&offset=0&ordering=-published_at")
@@ -18,12 +20,26 @@ const NewsPage: React.FC = () => {
   }, [])
   return (
     <div style={{ width: "100%" }}>
-      {/* You can delete this div if you want */}
-      <div style={{ marginBottom: "10px" }}>{/* Add Switch inside here */}</div>
+      {/* Switch */}
+      <div style={{ marginBottom: "10px" }}>
+        <Typography.Text>View as: </Typography.Text>
+        <Switch
+          checked={view === "table"}
+          onChange={(checked) => setView(checked ? "table" : "grid")}
+          checkedChildren="Table"
+          unCheckedChildren="Grid"
+        />
+        <Typography.Text style={{ whiteSpace: "pre" }}>  (Switch between Table and Grid view)</Typography.Text>
+      </div>
+      {/* End of Switch */}
       <Divider />
       <Typography.Title level={2}>Articles</Typography.Title>
+      {view === "grid" ? (
+        <ArticleList articles={articles} loading={loading} />
+      ) : (
+        <ArticleTable articles={articles} loading={loading} />
+      )}
       {/* Add conditional render logic for table vs grad/list */}
-      <ArticleList articles={articles} loading={loading} />
       {/* Add pagination control using Antd(lookup the component). The same one should be used for both the table and grid views */}
       {/* It should be centered on the page */}
       {/* When you change the page, or the items per page, it should reset the scroll to the top of the page */}
