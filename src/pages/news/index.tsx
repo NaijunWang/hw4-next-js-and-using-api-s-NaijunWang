4bd41477f97f6aa31a2268924c3c9956df9bf659
NaijunWang <wnj123e@gmail.com>
2025-10-16 20:45:06 -0400

    fix: add loading state to NewsPage to control ArticleList rendering

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -2,14 +2,17 @@
 import React, { useEffect, useState } from "react";
 import { Divider, Typography } from "antd";
 import { Article, ArticleResponse } from "@/types/types";
+import ArticleList from "@/components/ArticleList";
 
 const NewsPage: React.FC = () => {
   const [articles, setArticles] = useState<Article[]>([])
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     async function getArticles(){
       const res = await fetch("https://api.spaceflightnewsapi.net/v4/articles/?limit=10&offset=0&ordering=-published_at")
       const data: ArticleResponse = await res.json();
       setArticles(data.results);
+      setLoading(false);
     }; 
     getArticles();
   }, [])
@@ -20,14 +23,10 @@ const NewsPage: React.FC = () => {
       <Divider />
       <Typography.Title level={2}>Articles</Typography.Title>
       {/* Add conditional render logic for table vs grad/list */}
+      <ArticleList articles={articles} loading={loading} />
       {/* Add pagination control using Antd(lookup the component). The same one should be used for both the table and grid views */}
       {/* It should be centered on the page */}
       {/* When you change the page, or the items per page, it should reset the scroll to the top of the page */}
-        <ul>
-          {articles.map((a) => (
-            <li key={a.id}>{a.title}</li>
-          ))}
-      </ul>
     </div>
   );
 };
