9cf2005a93d36f263343bb7880bb48c666674bea
NaijunWang <wnj123e@gmail.com>
2025-10-16 21:16:46 -0400

    feat: finish ArticleTable component

--- src/components/ArticleTable.tsx
+++ src/components/ArticleTable.tsx
@@ -15,8 +15,43 @@ const ArticleTable: React.FC<ArticleTableProps> = ({ articles, loading }) => {
    * You will need to write a custom render function for the "published_at" column to format the date.
    *
    */
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title"
+    },
+    {
+      title: "News Source",
+      dataIndex: "news_site",
+      key: "news_site"
+    },
+    {
+      title: "Published At",
+      dataIndex: "published_at",
+      key: "published_at",
+      render: (date: string) => {
+        const formatted = new Date(date).toLocaleString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+        return formatted;
+      }
+    }
+  ]
+  return (
+    <Table 
+      columns={columns}
+      dataSource={articles}
+      loading={loading}
+      rowKey={(article) => article.id}
+      pagination={false}
+      bordered={false}
 
-  return <div>Build me</div>;
+    />
+  );
 };
 
 export default ArticleTable;
