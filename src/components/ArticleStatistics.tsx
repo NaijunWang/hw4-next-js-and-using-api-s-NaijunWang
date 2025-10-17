211037d395c46f6dc60f07962fb65df55e42961f
NaijunWang <wnj123e@gmail.com>
2025-10-16 23:03:50 -0400

    feat: finish ArticleStats component

--- src/components/ArticleStatistics.tsx
+++ src/components/ArticleStatistics.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, List, Typography } from "antd";
+import { Row, Col, List, Typography, Divider } from "antd";
 import { Article } from "@/types/types";
 
 interface ArticleStatisticsProps {
@@ -43,7 +43,79 @@ const ArticleStatistics: React.FC<ArticleStatisticsProps> = ({ articles }) => {
     ).toLocaleDateString(),
   ];
 
-  return <div>Build me</div>;
+  const featuredCount = articles.filter((a) => a.featured).length;
+
+  const dividerStyle = { margin: "10px 0", borderColor: "#e7e4e4ff", marginLeft: -12, marginRight: -12 };
+
+  return (
+    <div style={{ width: "100%", marginTop: 24 }}>
+      <Typography.Title level={2}>Article Statistics</Typography.Title>
+
+      <Row gutter={[8, 24]}>
+        <Col span={8}>
+          <div
+            style={{
+              border: "1px solid #d9d9d9",
+              borderRadius: 8,
+              padding: 12,
+            }}
+          >
+            <Typography.Text strong>Unique News Sources</Typography.Text>
+            <Divider style={dividerStyle} />
+            <List
+              dataSource={uniqueSources}
+              renderItem={(item, index) => (
+                <>
+                  <List.Item style={{ border: "none", padding: "4px 0" }}>
+                    <Typography.Text>{item}</Typography.Text>
+                  </List.Item>
+                  {index < uniqueSources.length - 1 && (
+                    <Divider style={dividerStyle} />
+                  )}
+                </>
+              )}
+            />
+          </div>
+        </Col>
+
+        <Col span={8}>
+          <div
+            style={{
+              border: "1px solid #d9d9d9",
+              borderRadius: 8,
+              padding: 12,
+            }}
+          >
+            <Typography.Text strong>Date Range of Articles</Typography.Text>
+            <Divider style={dividerStyle} />
+            <div style={{ marginTop: 4 }}>
+              <Typography.Text>Earliest: {dateRange[0]}</Typography.Text>
+            </div>
+            <Divider style={dividerStyle} />
+            <div style={{ marginTop: 4 }}>
+              <Typography.Text>Latest: {dateRange[1]}</Typography.Text>
+            </div>
+          </div>
+        </Col>
+
+        <Col span={8}>
+          <div
+            style={{
+              border: "1px solid #d9d9d9",
+              borderRadius: 8,
+              padding: 12,
+            }}
+          >
+            <Typography.Text strong>Number of Featured Articles</Typography.Text>
+            <Divider style={dividerStyle} />
+            <div style={{ marginTop: 4 }}>
+              <Typography.Text>Count: {featuredCount}</Typography.Text>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ArticleStatistics;
